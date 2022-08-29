755a1df281c50684f4b359dc0bef21ff43fb0a34
Priyanka-sharma181 <Priyanka-sharma181>
2022-08-29 17:09:08 +0530

    acknowldgement

--- public/js/chat.js
+++ public/js/chat.js
@@ -7,7 +7,12 @@ socket.on("message",(message)=>{
 document.querySelector('#message-form').addEventListener('submit',(e)=>{
     e.preventDefault()
     const message = e.target.elements.message.value
-    socket.emit('sendMessage',message)
+    socket.emit('sendMessage',message,(error)=>{
+        if(error){
+          return  console.log(error);
+        }
+        console.log("this msg was delevired", message);
+    })
 })
 
 // for location 
@@ -20,6 +25,8 @@ document.querySelector("#send-location").addEventListener('click',()=>{
         socket.emit("sendLocation",{
             latitude:position.coords.latitude,
             longitude:position.coords.longitude
+        },()=>{
+            console.log("location shared");
         })
     })
 })
--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 const path = require("path")
 const http = require("http")
+const Filter = require("bad-words")
 const socketio = require("socket.io")
 const express = require("express")
 const app =  express()
@@ -14,11 +15,17 @@ io.on('connection', (socket) => {
     socket.emit("message","welcome!")
 
 socket.broadcast.emit('message','A user has joined')
-    socket.on('sendMessage',(message)=>{
+    socket.on('sendMessage',(message,callback)=>{
+        const filter = new Filter()
+        if(filter.isProfane(message)){
+            return callback("profinity is not allowed")
+        }
         io.emit('message',message)
+        callback("Deleverd !")
     })
-    socket.on("sendLocation",(coords)=>{
+    socket.on("sendLocation",(coords,callback)=>{
         io.emit('message',`https://google.com/maps?q=${coords.latitude},${coords.longitude}`)
+       callback()
     })
     socket.on('disconnect',()=>{ 
         io.emit('message','A user has left')
